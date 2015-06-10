e97e2170f81f8f5d59e90c088c54a186cb564a07
TERRANZ <terranz@terranz.ath.cx>
2015-06-10 17:45:35 +0400

    dependencies

---
+++ modules/gui/src/main/java/ru/terra/l3db/gui/Main.java
@@ -0,0 +1,19 @@
+package ru.terra.l3db.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Date: 10.06.15
+ * Time: 17:42
+ */
+public class Main extends Application {
+    public static void main(String... args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
